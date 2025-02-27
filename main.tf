8fba1986b04af5ad31a5455b716f0190c80b82b5
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 17:02:40 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 #}
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-ECS-presnetation"
+  bucket = "terraform-ecs-presnetation"
 
   versioning {
     enabled = true

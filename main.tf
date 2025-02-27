2a407ce6453f7510fcd9e40b352d7472e802744d
Nikhil Kumar <nikhil@Nikhils-MacBook-Pro.local>
2025-02-27 16:42:46 -0600

    Terraform state file update

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-ECS-presnetation"
+    key    = "Aws-default-infrastructure"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-ECS-presnetation"
+
+  versioning {
+    enabled = true
+  }
+}
+

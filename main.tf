ffbda712ad347e674d67b534db85a8a2dbdc64d6
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 17:18:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-ecs-presnetation"
-#    key    = "Aws-default-infrastructure"
-#    region = "us-east-2"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "terraform-ecs-presnetation"
+    key    = "Aws-default-infrastructure"
+    region = "us-east-2"
+  }
+}
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-ecs-presnetation"

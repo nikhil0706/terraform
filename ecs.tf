353e9be1fa33a31b1b6cd8102bce2d108aef5504
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 18:46:17 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -7,14 +7,14 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-2"
+  #availability_zone = "us-east-2"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-2"
+  #availability_zone = "us-east-2"
   map_public_ip_on_launch = true
 }
 

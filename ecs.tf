7fdc486852725fe1f39c64acd2a6fad58d2cade3
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 18:34:44 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -4,14 +4,14 @@ resource "aws_vpc" "main" {
 }
 
 # Create Public Subnets
-resource "aws_subnet" "public-1" {
+resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "public-2" {
+resource "aws_subnet" "public_2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2"
@@ -37,7 +37,7 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs_sg.id]
-  subnets            = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 }
 
 # Target Group

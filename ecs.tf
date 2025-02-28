773cd7807bf8d93b3351f6b99f592deb2167d9e6
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 18:14:31 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -79,6 +79,8 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  name = "my-ecs_task_execution_role_policy_attachment"
+
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   roles      = [aws_iam_role.ecs_task_execution_role.name]
 }

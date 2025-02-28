6cd3cd2daaf5a3ad5ed9469eb1359fee7361992b
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 18:43:17 -0600

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-2"
+}
+
+#variable "instance_type" {
+#  description = "The EC2 instance type"
+#  type        = string
+#  default     = "t2.micro"
+#}
+
+#variable "availability_zone" {
+#  description = "The Availability Zone to deploy the instance in"
+#  type        = string
+#  default     = "us-west-2a"
+#}
+
+#variable "vpc_cidr" {
+#  description = "The cidr block of VPC"
+#  type        = string
+#  default     = "10.123.0.0/16"
+
+#}
+
+#variable "subnet_cidr" {
+#  description = "The cidr block of Subnet"
+#  type        = string
+#  default     = "10.123.1.0/24"
+
+#}

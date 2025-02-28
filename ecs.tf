3509209352fbf78a42946d17a5615867cd8ac374
nikhil0706 <nikhil.devraj77@gmail.com>
2025-02-27 18:32:10 -0600

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -4,14 +4,14 @@ resource "aws_vpc" "main" {
 }
 
 # Create Public Subnets
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2"

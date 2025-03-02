6d9d0970b85da6bdf45c0e3159010135d60a69e0
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-02 16:15:40 -0600

    Update ecs.tf

--- environments/dev/ecs.tf
+++ environments/dev/ecs.tf
@@ -108,7 +108,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.ecs_pubsubnet.id  # Use the public subnet here
+  subnet_id     = [aws_subnet.ecs_pubsubnet1.id , aws_subnet.ecs_pubsubnet2.id ]  # Use the public subnet here
 }
 
 #########public end

d2b7fc270904607f4d8048c5c53cf56e33b096c6
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-03 13:44:19 -0600

    Update tests.tf

--- environments/dev/tests.tf
+++ environments/dev/tests.tf
@@ -1,24 +1,4 @@
-resource "aws_s3_bucket" "nik-dem-ecs" {
-  bucket = "my-nik-dem-ecs-bucket"
-  acl    = "private"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "nik-dem-ecs" {
-  bucket = aws_s3_bucket.nik-dem-ecs.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
 
 resource "aws_s3_bucket_object" "test" {
   bucket = aws_s3_bucket.nik-dem-ecs.bucket

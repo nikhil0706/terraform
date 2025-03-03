32aa8518255a641aad810a654cb1fda6ccf0afa4
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-03 11:05:45 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,22 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = var.aws_region
+#provider "aws" {
+#  version = "~> 2.0"
+#  region  = var.aws_region
+#}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"  # ✅ Correct way to specify version
+    }
+  }
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
 
 
 terraform {
@@ -11,6 +26,8 @@ terraform {
     region = "us-east-2"
   }
 }
+
+
 
 #resource "aws_s3_bucket" "terraform_state" {
 #  bucket = "terraform-ecs-presnetation"

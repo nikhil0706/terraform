1b8bc8c2eed9042c24bb83e2ed5f9449bdfbb722
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-03 13:05:52 -0600

    Update ecscloudwatch.tf

--- environments/dev/ecscloudwatch.tf
+++ environments/dev/ecscloudwatch.tf
@@ -56,7 +56,6 @@ resource "aws_cloudwatch_metric_alarm" "log_error_alarm" {
   alarm_name          = "log-error-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
-  alarm_type          = "MetricAlarm"
   metric_name         = "IncomingLogEvents"
   namespace           = "AWS/Logs"
   period              = 30  # 30sec

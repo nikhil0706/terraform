21e252e62389e841cab364fa678f6b5a6ef0272f
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-03 02:23:19 -0600

    Update ecs.tf

--- environments/dev/ecs.tf
+++ environments/dev/ecs.tf
@@ -346,14 +346,14 @@ resource "aws_ecs_task_definition" "app_task" {
    #   retries     = 3
    #   startPeriod = 60
    # }
-  #logConfiguration = {
-  #    logDriver = "awslogs"
-  #    options = {
-  #      awslogs-group         = aws_cloudwatch_log_group.ecs_log_group.name
-  #      awslogs-region        = "us-east-2"  # Replace with your region
-  #      awslogs-stream-prefix = "demo-app"
-  #    }
-  #  }
+  logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.ecs_log_group.name
+        awslogs-region        = "us-east-2"  # Replace with your region
+        awslogs-stream-prefix = "demo-app"
+      }
+    }
   }])
 }
 
@@ -383,42 +383,3 @@ resource "aws_ecs_service" "app_service" {
   depends_on = [aws_lb_listener.front_end]
 }
 
-output "load_balancer_url" {
-  description = "The URL of the Application Load Balancer"
-  value       = aws_lb.app_lb.dns_name
-}
-
-output "vpc_id" {
-  description = "The ID of the created VPC"
-  value       = aws_vpc.main.id
-}
-
-output "subnet_ids" {
-  description = "The IDs of the created subnets"
-  value       = [aws_subnet.ecs_subnet_1.id , aws_subnet.ecs_subnet_1.id]
-}
-
-output "security_group_id" {
-  description = "The ID of the security group"
-  value       = aws_security_group.ecs_secgrp.id
-}
-
-output "ecs_cluster_id" {
-  description = "The ID of the ECS Cluster"
-  value       = aws_ecs_cluster.ecs_cluster.id
-}
-
-output "task_definition_arn" {
-  description = "The ARN of the ECS Task Definition"
-  value       = aws_ecs_task_definition.app_task.arn
-}
-
-output "ecr_image_url" {
-  description = "ECR repository URL for the image"
-  value       = "${data.aws_ecr_repository.app_repo.repository_url}:latest"
-}
-
-output "alb_url" {
-  description = "Application load balancer "
-  value       = aws_lb.app_lb.dns_name
-}

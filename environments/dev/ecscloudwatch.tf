e34532efb37121c56869a1d7ef72cc5982165aae
nikhil0706 <nikhil.devraj77@gmail.com>
2025-03-03 10:26:35 -0600

    Update ecscloudwatch.tf

--- environments/dev/ecscloudwatch.tf
+++ environments/dev/ecscloudwatch.tf
@@ -3,30 +3,6 @@ resource "aws_cloudwatch_log_group" "ecs_log_group" {
   retention_in_days = 7  # Optional: Retain logs for 7 days
 }
 
-#  IAM Role for ECS Task Execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-
 # IAM Policy to Allow Logging (Optional if using Execution Role Policy)
 
 resource "aws_iam_policy" "cloudwatch_logs_policy" {
